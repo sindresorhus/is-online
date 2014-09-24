f238a6721b26efd0e4e79d709631dfc41757a737
silverwind <me@silverwind.io>
2014-09-24 19:29:53 +0200

    Quote style, simpler socket timeout handling

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ var timeout = 1000;
 
 // root hints from http://www.internic.net/domain/named.root
 var roots = [
-	"198.41.0.4",
-	"192.228.79.201",
-	"192.33.4.12",
-	"199.7.91.13",
-	"192.203.230.10",
-	"192.5.5.241",
-	"192.112.36.4",
-	"128.63.2.53",
-	"192.36.148.17",
-	"192.58.128.30",
-	"193.0.14.129",
-	"199.7.83.42",
-	"202.12.27.33"
+	'128.63.2.53',
+	'192.112.36.4',
+	'192.203.230.10',
+	'192.228.79.201',
+	'192.33.4.12',
+	'192.36.148.17',
+	'192.5.5.241',
+	'192.58.128.30',
+	'193.0.14.129',
+	'198.41.0.4',
+	'199.7.83.42',
+	'199.7.91.13',
+	'202.12.27.33'
 ];
 
 module.exports = function (domains, cb) {
@@ -44,7 +44,7 @@ module.exports = function (domains, cb) {
 
 	var req = dns.Request({
 		question: dns.Question({
-			name: "com",
+			name: 'com',
 			type: 'NS'
 		}),
 		server: {
@@ -65,18 +65,18 @@ module.exports = function (domains, cb) {
 			cb(null, true);
 		} else {
 			// Either DNS intercepting is in place or the response in mangled,
-			// try connecting to our domains on port 80.
+			// try connecting to our domains on port 80, and if one handshake
+			// succeeds, we're definately online.
 			eachAsync(domains, function (domain, i, next) {
 				var socket = new net.Socket();
-				socket.setTimeout(timeout, function () {
+				socket.setTimeout(timeout);
+				socket.on('error', function () {
 					socket.destroy();
-				});
-				socket.on('error', function(err) {
 					next();
-					socket.destroy();
 				});
 				socket.connect(80, domain, function () {
 					cb(null, true);
+					socket.end();
 					next(new Error()); // skip to end
 				});
 			}, function () {
