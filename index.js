4e0dfc7d8d9ddce2d4b75bd6b2d02dbf8f5d00f5
silverwind <me@silverwind.io>
2014-09-30 01:57:57 +0200

    Close GH-5: Root DNS and socket checks. Fixes #4, Fixes #4

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
-var dns = require('dns');
+var dns = require('native-dns');
+var net = require('net');
 var eachAsync = require('each-async');
 var onetime = require('onetime');
+var roots = require('root-hints')('A');
+
+var timeout = 1000;
 
 module.exports = function (domains, cb) {
 	if (typeof domains === 'function') {
@@ -19,17 +23,50 @@ module.exports = function (domains, cb) {
 
 	cb = onetime(cb);
 
-	eachAsync(domains, function (domain, i, next) {
-		dns.lookup(domain, 4, function (err) {
-			if (!err) {
-				cb(null, true);
-				next(new Error); // skip to end
-				return;
-			}
+	// pick a random root server to query
+	var server = roots[Math.floor(Math.random() * roots.length)];
 
-			next();
-		});
-	}, function () {
+	var req = dns.Request({
+		question: dns.Question({
+			name: 'com',
+			type: 'NS'
+		}),
+		server: {
+			address: server
+		},
+		timeout: timeout
+	});
+
+	req.on('timeout', function () {
+		// We ran into the timeout, we're offline with high confidence
 		cb(null, false);
 	});
+
+	req.on('message', function (err, answer) {
+		if (answer.authority.length && answer._socket.address === server) {
+			// We got an answer and the source matches the queried server,
+			// we're online with high confidence
+			cb(null, true);
+		} else {
+			// Either DNS intercepting is in place or the response in mangled,
+			// try connecting to our domains on port 80, and if one handshake
+			// succeeds, we're definately online.
+			eachAsync(domains, function (domain, i, next) {
+				var socket = new net.Socket();
+				socket.setTimeout(timeout);
+				socket.on('error', function () {
+					socket.destroy();
+					next();
+				});
+				socket.connect(80, domain, function () {
+					cb(null, true);
+					socket.end();
+					next(new Error()); // skip to end
+				});
+			}, function () {
+				cb(null, false);
+			});
+		}
+	});
+	req.send();
 };
