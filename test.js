0b21fa28e20560b71da518ca7b12444f3a6a9c84
Nuno Morais <geral@nuno-morais.eu>
2016-12-14 00:09:21 +0000

    updated tests

--- test.js
+++ test.js
@@ -2,14 +2,14 @@
 var test = require('ava');
 var isOnline = require('./');
 
-test(async (t) => {
-	isOnline()
-		.then( (online) => { t.true(online); })
+test((t) => {
+	return isOnline()
+		.then((online) => { t.true(online); })
 });
 
-test(async (t) => {
-	isOnline({ timeout: 500 })
-		.then( (online) => { t.true(online); })
+test((t) => {
+	return isOnline({ timeout: 500 })
+		.then((online) => { t.true(online); })
 });
 
 test(async (t) => {
