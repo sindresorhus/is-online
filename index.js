929cdc89d1e201721ced095a5ea700218f2012b2
Aditya Patadia <aditya@patadia.org>
2019-08-18 00:08:57 +0530

    if one check is successful, other checks are now cancelled

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
 'use strict';
 const got = require('got');
 const publicIp = require('public-ip');
-const pAny = require('p-any');
 const pTimeout = require('p-timeout');
 
-const appleCheck = async options => {
-	const {body} = await got('http://captive.apple.com/hotspot-detect.html', {
+const appleCheck = options => {
+	const gotPromise = got('http://captive.apple.com/hotspot-detect.html', {
 		family: options.version === 'v4' ? 4 : 6,
 		headers: {
 			'user-agent': 'CaptiveNetworkSupport/1.0 wispr'
 		}
 	});
 
-	return /Success/.test(body || '') || Promise.reject();
+	const promise = gotPromise.then(res => {
+		return /Success/.test(res.body || '') || Promise.reject();
+	}).catch(error => {
+		if (!(error instanceof got.CancelError)) {
+			throw error;
+		}
+	});
+
+	promise.cancel = gotPromise.cancel;
+
+	return promise;
 };
 
-const isOnline = options => {
+const isOnline = async options => {
 	options = {
 		timeout: 5000,
 		version: 'v4',
 		...options
 	};
 
-	const promise = pAny([
-		(async () => {
-			await publicIp[options.version]();
-			return true;
-		})(),
-		(async () => {
-			await publicIp[options.version]({https: true});
-			return true;
-		})(),
-		appleCheck(options)
+	const dnsPromise = publicIp[options.version]();
+
+	const httpsPromise = publicIp[options.version]({
+		https: true
+	});
+
+	const applePromise = appleCheck(options);
+
+	const promise = Promise.race([
+		dnsPromise,
+		httpsPromise,
+		applePromise
 	]);
 
-	return pTimeout(promise, options.timeout).catch(() => false);
+	try {
+		await pTimeout(promise, options.timeout);
+		dnsPromise.cancel();
+		httpsPromise.cancel();
+		applePromise.cancel();
+		return true;
+	} catch (error) {
+		return false;
+	}
 };
 
 module.exports = isOnline;
